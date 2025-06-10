36cf6f41d9757064ae32203c209cb54cf51446cc
rishikesh.ghemud <rishikesh.ghemud.work1@gmail.com>
2025-06-10 23:50:12 +0530

    Added wouter for routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import Login from './Auth/Login.tsx'
+import "./app.css"
+import HomePage from './HomePage.tsx'
+import { Switch, Route } from 'wouter'
 
 function App() {
 
   return (
     <>
-      <Login />
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/" component={HomePage} />
+    </Switch>
     </>
   )
 }
